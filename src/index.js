d16e1b34455a988946c543d2f0dbcf9b38913b07
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-07-26 00:26:15 +0200

    Add new city

--- src/index.js
+++ src/index.js
@@ -10,5 +10,6 @@ root.render(
         <Meteo city="Nancy" zipcode="54000" countrycode="fr" />
         <Meteo city="Toulouse" zipcode="31000" countrycode="fr" />
         <Meteo city="Le Lamentin" zipcode="97232" countrycode="mq"/>
+        <Meteo zipcode="07086" city="Weehawken" countrycode="US" />
     </React.StrictMode>
 );
